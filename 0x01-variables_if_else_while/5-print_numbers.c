99ef683c82aacd42c8cc7413ecd20d75d4b5c408
EllisOtoo <ellisotoo@gmail.com>
2023-07-16 03:54:35 -0700

    Solution updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int start = 0;
+    while (start < 10)
+    {
+        printf("%d", start);
+        start++;
+    }
+    printf("\n");
+    return (0);
+}
