82a038f4529d8c312370243bc98f60228f537a3e
EllisOtoo <ellisotoo@gmail.com>
2023-07-17 14:25:37 -0700

    Void added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
     fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return 1;
