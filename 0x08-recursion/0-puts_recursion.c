1d81e9af3a808897dfd6e8ad662c39ed3f842edb
EllisOtoo <ellisotoo@gmail.com>
2023-08-03 20:37:05 -0700

    solutoons to 0-puts added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ *A function thatb prints string followed by a new line
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This projetc is about recurison
